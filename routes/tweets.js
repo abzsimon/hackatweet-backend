e0e8aadfec35b59365a918d5c17d0ff0f4fdb2cd
Simon Apartis <simon.apartis@cnrs.fr>
2024-12-19 20:48:00 +0100

    test

--- routes/tweets.js
+++ routes/tweets.js
@@ -22,4 +22,27 @@ router.post('/', (req, res) => {
     });
 });
 
+router.get('/all', (req, res) => {
+    User.findOne({ token: req.body.token }).then((user) => {
+        if (user === null) {
+            res.json({ result: false, error: 'No user specified!' });
+        } else {
+            Tweet.find().populate('author', ['username']).then(data => {
+                res.json({ result: true, tweets: data });
+            });
+        }
+    });
+})
+
+router.delete('/delete', (req, res) => {
+    const findTweet = Tweet.(e => e.author === req.body.author);
+
+    if (searchedWeather) {
+        weather = weather.filter(e => e.cityName !== req.params.cityName);
+        res.json({ result: true, weather });
+    } else {
+        res.json({ result: false, error: 'City not found' });
+    }
+});
+
 module.exports = router;
